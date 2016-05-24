b85cc0eee3f85605de654ebefad797df8a83e597
hanshu <hanshu@>
2016-05-24 19:20:23 +0800

    a

--- bloomberg_bw.py
+++ bloomberg_bw.py
@@ -1,7 +1,9 @@
 import requests
 import json
+import os
 url='http://mobile.bbwc.cn/'
-def load_index(cats,stamp):
+
+def fetch_index(cats,stamp):
     for cat in cats:        
         fname='index_{cat}_{stamp}.json'.format(cat=cat,stamp=stamp)    
         url='http://content.cdn.bb.bbwc.cn/slateInterface/v7/app_1/android/tag/cat_{cat}/articlelist?updatetime={stamp}'.format(cat=cat,stamp=stamp)
@@ -11,7 +13,7 @@ def load_index(cats,stamp):
             print fname
             continue
         with open('data/'+fname,'w') as fh:
-            fh.write(json.dumps(jso,indent=1))
+            fh.write(json.dumps(jso))
             
 def u2g(st):return st.encode('gbk','ignore')
 
@@ -25,12 +27,30 @@ def proc_index(cats,stamp):
             dc[ arti['articleid'] ] = [arti['title'],arti['weburl'] ]
         # print json.dumps(jso,indent=1)
         # exit()
-    for k in sorted(dc.keys(),reverse=True):
-        print k,u2g('\t'.join(dc[k]))
+    for aid in sorted(dc.keys(),reverse=True)[:]:
+        print aid,u2g('\t'.join(dc[aid]))
+        continue
         
+def article_fetch(aid):
+    url='http://content.cdn.bb.bbwc.cn/slateInterface/v6/app_1/android/article/{aid}?fetch_all=1&t=813377&related=1'.format(aid=aid)
+    fname='data/article/'+'article_%s.json'%aid
+    if os.path.exists(fname):        
+        return
+    r=requests.get(url)
+    if r.status_code==404:
+        print '404',url
+        return
+    with open(fname,'w') as fh:
+        fh.write( json.dumps(r.json() ))
+        
+def loop_many():
+    for i in range(5000):
+        article_fetch(10049745+i)
+    
 if __name__=='__main__':
     cats=map(str,range(11,22))
-    stamp='1464061292'
-    # load_index(cats,stamp)
-    proc_index(cats,stamp)
+    stamp='10053004'
+    # fetch_index(cats,stamp)
+    # proc_index(cats,stamp)
+    loop_many()
     
