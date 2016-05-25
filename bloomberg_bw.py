d3f47f8fdf8a20501f3775f4696dfa74f641edaf
hanshu <hanshu@>
2016-05-25 15:51:43 +0800

    add jieba wordseg

--- bloomberg_bw.py
+++ bloomberg_bw.py
@@ -1,6 +1,13 @@
 import requests
 import json
 import os
+import glob
+import re
+import jieba
+import jieba.analyse
+jieba.initialize()
+from collections import Counter
+
 url='http://mobile.bbwc.cn/'
 
 def fetch_index(cats,stamp):
@@ -36,21 +43,41 @@ def article_fetch(aid):
     fname='data/article/'+'article_%s.json'%aid
     if os.path.exists(fname):        
         return
-    r=requests.get(url)
+    try:r=requests.get(url)
+    except:
+        print 'Fail:',url
+        return
     if r.status_code==404:
-        print '404',url
+        print '404:',url
         return
     with open(fname,'w') as fh:
         fh.write( json.dumps(r.json() ))
+        print 'succ:',url
+        return
         
+def print_article():
+    pat=re.compile('<[^>]+>')
+    cnt=Counter()
+    for file in glob.glob('data/article/*.json'):
+        with open(file) as fh:
+            try:jso=json.load(fh )
+            except:print 'LoadFail:',file
+            # print json.dumps(jso,indent=1)
+            art=jso['article'][0]
+            title= art['title']
+            content=pat.sub('',art['content'])
+            print u2g(title)
+            tags=jieba.analyse.extract_tags(content, topK=50)
+            print u2g(','.join( tags ) )
+
 def loop_many():
     for i in range(5000):
-        article_fetch(10049745+i)
+        article_fetch(10061614+i)
     
 if __name__=='__main__':
     cats=map(str,range(11,22))
-    stamp='10053004'
+    stamp='1464143056440'
     # fetch_index(cats,stamp)
     # proc_index(cats,stamp)
-    loop_many()
-    
+    # loop_many()
+    print_article()
